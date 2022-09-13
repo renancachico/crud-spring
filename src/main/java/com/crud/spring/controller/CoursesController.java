e5a6a5897a09e740b3c298fcb908d7b26fa4d2e6
rsantos68 <rsantos68@dxc.com>
2022-09-13 15:04:39 -0300

    Criando o CREATE da tela de cursos.

--- src/main/java/com/crud/spring/controller/CoursesController.java
+++ src/main/java/com/crud/spring/controller/CoursesController.java
@@ -2,9 +2,14 @@ package com.crud.spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.spring.model.Course;
@@ -22,5 +27,11 @@ public class CoursesController {
 	@GetMapping
 	public @ResponseBody List<Course> list(){
 		return courseRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Course create(@RequestBody Course course) {
+		return courseRepository.save(course);
 	}
 }
