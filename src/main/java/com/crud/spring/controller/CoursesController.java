42c1310977e75dd1b7dbf7c299cfdfc81d6ed37c
rsantos68 <rsantos68@dxc.com>
2022-09-16 10:35:41 -0300

    Atualizar Curso: API Spring HTTP PUT

--- src/main/java/com/crud/spring/controller/CoursesController.java
+++ src/main/java/com/crud/spring/controller/CoursesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,12 +33,26 @@ public class CoursesController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Course> findById(@PathVariable Long id){
-		return courseRepository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
+		return courseRepository.findById(id)
+				.map(recordFound -> ResponseEntity.ok().body(recordFound))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Course create(@RequestBody Course course) {
 		return courseRepository.save(course);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+		return courseRepository.findById(id)
+				.map(recordFound -> {
+					recordFound.setName(course.getName());
+					recordFound.setCategory(course.getCategory());
+					Course updated = courseRepository.save(recordFound);
+					ResponseEntity.ok().body(updated);
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
